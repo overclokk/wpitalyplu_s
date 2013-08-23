7475fea3c0e413c0a8af1effa7eeb81b68ac6fe6
EugenioPetulla <eugeniopetulla@yahoo.it>
2013-08-23 19:19:55 +0200

    Function For Pure Menu Selected item

--- functions.php
+++ functions.php
@@ -123,3 +123,13 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/*add class to style active menu links*/
+
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+function special_nav_class($classes, $item){
+     if( in_array('current-menu-item', $classes) ){
+             $classes[] = 'pure-menu-selected ';
+     }
+     return $classes;
+}
