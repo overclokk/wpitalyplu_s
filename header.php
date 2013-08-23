f6490b8b3108bb8ee3739953ddabdccdb410a758
EugenioPetulla <eugeniopetulla@yahoo.it>
2013-08-23 15:36:19 +0200

    First Upload

---
+++ header.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <main id="main">
+ *
+ * @package wpitalyplu_s
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.1/pure-min.css">
+<script src="http://yui.yahooapis.com/3.11.0/build/yui/yui-min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,700italic' rel='stylesheet' type='text/css'>
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="pure-skin" class="pure-skin-wpitalyplus">
+<div id="page" class="hfeed site">
+	<?php do_action( 'before' ); ?>
+<header id="masthead" class="site-header" role="banner">
+		<!--<div class="site-branding">
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		</div>-->
+
+		<nav id="site-navigation" class="pure-menu pure-menu-open pure-menu-fixed pure-menu-horizontal" role="navigation">
+			<h1 class="site-title pure-menu-heading"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+			<?php //set menu's elements
+						$defaults = array(
+							'theme_location'  => 'primary',
+							'menu'            => '',
+							'container'       => '',
+							'container_class' => '',
+							'container_id'    => '',
+							'menu_class'      => '',
+							'menu_id'         => '',
+							'echo'            => true,
+							'fallback_cb'     => 'wp_page_menu',
+							'before'          => '',
+							'after'           => '',
+							'link_before'     => '',
+							'link_after'      => '',
+							'items_wrap'      => '<ul id="pure-nav">%3$s</ul>',
+							'depth'           => 0,
+							'walker'          => ''
+						);
+
+						wp_nav_menu( $defaults ); //call menu here
+
+						?>
+						<!--<script>
+YUI({
+    classNamePrefix: 'pure'
+}).use('gallery-sm-menu', function (Y) {
+
+    var horizontalMenu = new Y.Menu({
+        container         : '#site-navigation',
+        sourceNode        : '#pure-nav',
+        orientation       : 'horizontal',
+        hideOnOutsideClick: false,
+        hideOnClick       : false
+    });
+
+    horizontalMenu.render();
+    horizontalMenu.show();
+
+});
+</script>-->
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
---
+++ searchform.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying search forms in wpitalyplu_s
+ *
+ * @package wpitalyplu_s
+ */
+?>
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'wpitalyplu_s' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'wpitalyplu_s' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="<?php _ex( 'Search for:', 'label', 'wpitalyplu_s' ); ?>">
+	</label>
+	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'wpitalyplu_s' ); ?>">
+</form>
