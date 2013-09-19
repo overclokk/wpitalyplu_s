be45802d9d20aeb00a3089047eb75e4d506b8c0a
EugenioPetulla <eugeniopetulla@yahoo.it>
2013-09-19 06:53:24 +0200

    Fixed search no-result page

--- no-results.php
+++ no-results.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-<section class="no-results not-found">
+<section class="no-results not-found postcard-container">
 	<header class="page-header">
 		<h1 class="page-title"><?php _e( 'Nothing Found', 'wpitalyplu_s' ); ?></h1>
 	</header><!-- .page-header -->
